86c1f89ed36bf85ab9238da63d8e212c6939da72
A K SINGH <adityendra97@gmail.com>
2021-02-25 08:27:46 +0530

    Initial Commit

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,42 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for, current_app, render_template
+from flask_mail import Message
+from flaskblog import mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        current_app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    subject = "Reset your Password"
+    link = url_for('users.reset_token', token=token, _external=True)
+    template = render_template(
+            'email_template.html',
+            link=link)
+    print(template)
+    msg.html = template
+#     msg.html = f'''<h1>Reset your Password</h1>
+# <p>To reset your password, visit the following link:</p>
+# {url_for('users.reset_token', token=token, _external=True)}
+# <p>If you did not make this request then simply ignore this email and no changes will be made.</p>
+# '''
+    mail.send(msg)
+    
